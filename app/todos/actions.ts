cc8aee7a6686447bdcdbab2904c50c35936e52c2
jolbol1 <github@jamesshopland.com>
2024-04-21 12:20:39 +0100

    Step 5 - Update and Delete

--- app/todos/actions.ts
+++ app/todos/actions.ts
@@ -1,5 +1,6 @@
 "use server"
 
+import { Todo } from "@/types/custom";
 import { createClient } from "@/utils/supabase/server";
 import { revalidatePath } from "next/cache";
 
@@ -24,6 +25,48 @@ export async function addTodo(formData: FormData) {
 
     if (error) {
         throw new Error("Error adding task")
+    }
+
+    revalidatePath("/todos")
+}
+
+export async function deleteTodo(id: number) {
+    const supabase = createClient();
+    const { data: { user } } = await supabase.auth.getUser()
+
+    if (!user) {
+        throw new Error("User is not logged in")
+    }
+
+    const { error } = await supabase.from("todos").delete().match({
+        user_id: user.id,
+        id: id
+    })
+
+    if (error) {
+        throw new Error("Error deleting task")
+    }
+
+    revalidatePath("/todos")
+}
+
+export async function updateTodo(todo: Todo) {
+
+
+    const supabase = createClient();
+    const { data: { user } } = await supabase.auth.getUser()
+
+    if (!user) {
+        throw new Error("User is not logged in")
+    }
+
+    const { error } = await supabase.from("todos").update(todo).match({
+        user_id: user.id,
+        id: todo.id
+    })
+
+    if (error) {
+        throw new Error("Error updating task")
     }
 
     revalidatePath("/todos")
