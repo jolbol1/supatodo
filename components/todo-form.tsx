6768590561b37faebc4764aefc529e728064bfb3
jolbol1 <github@jamesshopland.com>
2024-04-21 12:24:27 +0100

    Step 6 - useFormStatus

--- components/todo-form.tsx
+++ components/todo-form.tsx
@@ -5,17 +5,20 @@ import { Card, CardContent } from "@/components/ui/card";
 import { Textarea } from "@/components/ui/textarea";
 import { Send } from "lucide-react";
 import { useRef } from "react";
+import { useFormStatus } from "react-dom";
 
 function FormContent() {
+  const { pending } = useFormStatus();
   return (
     <>
       <Textarea
+        disabled={pending}
         minLength={4}
         name="todo"
         required
         placeholder="Add a new todo"
       />
-      <Button type="submit" size="icon" className="min-w-10">
+      <Button type="submit" size="icon" className="min-w-10" disabled={pending}>
         <Send className="h-5 w-5" />
         <span className="sr-only">Submit Todo</span>
       </Button>
--- components/todo-item.tsx
+++ components/todo-item.tsx
@@ -6,6 +6,7 @@ import { Checkbox } from "@/components/ui/checkbox";
 import { cn } from "@/lib/utils";
 import { Todo } from "@/types/custom";
 import { Trash2 } from "lucide-react";
+import { useFormStatus } from "react-dom";
 
 export function TodoItem({ todo }: { todo: Todo }) {
   return (
@@ -16,11 +17,13 @@ export function TodoItem({ todo }: { todo: Todo }) {
 }
 
 export function TodoCard({ todo }: { todo: Todo }) {
+  const { pending } = useFormStatus();
   return (
-    <Card className={cn("w-full")}>
+    <Card className={cn("w-full", pending && "opacity-50")}>
       <CardContent className="flex items-start gap-3 p-3">
         <span className="size-10 flex items-center justify-center">
           <Checkbox
+            disabled={pending}
             checked={Boolean(todo.is_complete)}
             onCheckedChange={async (val) => {
               if (val === "indeterminate") return;
@@ -30,6 +33,7 @@ export function TodoCard({ todo }: { todo: Todo }) {
         </span>
         <p className={cn("flex-1 pt-2 min-w-0 break-words")}>{todo.task}</p>
         <Button
+          disabled={pending}
           formAction={async (data) => {
             await deleteTodo(todo.id);
           }}
