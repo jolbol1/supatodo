7c9606efc000c017b8221f3c5dfb6cddd3b11c6b
Anish Dangol <dangol.anish001@gmail.com>
2024-06-08 19:40:14 +0545

    added update and delte

--- components/todo-form.tsx
+++ components/todo-form.tsx
@@ -5,17 +5,22 @@ import { Card, CardContent } from "@/components/ui/card";
 import { Textarea } from "@/components/ui/textarea";
 import { Send } from "lucide-react";
 import { useRef } from "react";
+import { useFormStatus } from "react-dom";
+import { TodoOptimisticUpdate } from "./todo-list";
+import { Todo } from "@/types/custom";
 
 function FormContent() {
+  const { pending } = useFormStatus();
   return (
     <>
       <Textarea
+        disabled={pending}
         minLength={4}
         name="todo"
         required
         placeholder="Add a new todo"
       />
-      <Button type="submit" size="icon" className="min-w-10">
+      <Button disabled={pending} type="submit" size="icon" className="min-w-10">
         <Send className="h-5 w-5" />
         <span className="sr-only">Submit Todo</span>
       </Button>
@@ -23,7 +28,11 @@ function FormContent() {
   );
 }
 
-export function TodoForm() {
+export function TodoForm({
+  optimisticUpdate,
+}: {
+  optimisticUpdate: TodoOptimisticUpdate;
+}) {
   const formRef = useRef<HTMLFormElement>(null);
   return (
     <Card>
@@ -32,6 +41,17 @@ export function TodoForm() {
           ref={formRef}
           className="flex gap-4"
           action={async (data) => {
+            const newTodo: Todo = {
+              id: -1,
+              inserted_at: "",
+              user_id: "",
+              task: data.get("todo") as string,
+              is_complete: false,
+            };
+            optimisticUpdate({
+              action: "create",
+              todo: newTodo,
+            });
             await addTodo(data);
             formRef.current?.reset();
           }}
