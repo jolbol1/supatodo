e16408c96e48c2a077616db0d3ca1ef2d2ab69b1
Anish Dangol <dangol.anish001@gmail.com>
2024-06-08 16:03:11 +0545

    added auth

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -8,12 +8,24 @@ import {
 } from "@/components/ui/card";
 import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
+import { emailLogin, signup } from "./actions";
+import { createClient } from "@/utils/supabase/server";
+import { redirect } from "next/navigation";
 
-export default function Login({
+export default async function Login({
   searchParams,
 }: {
   searchParams: { message: string };
 }) {
+  const supabase = await createClient();
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  if (user) {
+    return redirect("/todos");
+  }
   return (
     <section className="h-[calc(100vh-57px)] flex justify-center items-center">
       <Card className="mx-auto max-w-sm">
@@ -52,11 +64,13 @@ export default function Login({
                 {searchParams.message}
               </div>
             )}
-            <Button className="w-full">Login</Button>
+            <Button formAction={emailLogin} className="w-full">
+              Login
+            </Button>
           </form>
           <div className="mt-4 text-center text-sm">
             Don&apos;t have an account?{" "}
-            <button form="login-form" className="underline">
+            <button formAction={signup} form="login-form" className="underline">
               Sign up
             </button>
           </div>
